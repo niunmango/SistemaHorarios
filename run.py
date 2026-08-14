d5e84f3609a37191703d6d1f43e0e4a32c8d2289
Ramiro García Poggi <ramiro.garcia@fi.uncoma.edu.ar>
2026-08-14 19:47:24 -0300

    fix(dev): activar debug y auto-reloader por defecto en run.py para desarrollo local

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ from app import create_app, app
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    debug = os.environ.get('FLASK_DEBUG', 'False').lower() in ['true', '1']
+    debug = os.environ.get('FLASK_DEBUG', 'true').lower() in ['true', '1']
     app.run(host='0.0.0.0', port=port, debug=debug)
